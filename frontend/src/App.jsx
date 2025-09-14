a843add575a3960f10d4ea4c62f6d8afc009df1c
vinothbhc1986 <vinothbhc1986@gmail.com>
2025-09-14 15:20:25 +0530

    add voumes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API = `/tasks`;
+const API = `/api/tasks`;
 
 function App() {
   const [tasks, setTasks] = useState([]);
